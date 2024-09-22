ffc140c725dd43d86adea83a1712ad68a2445d15
byte2702 <weschenfelder@gmx.de>
2024-09-22 17:10:56 +0200

    Implementation.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 namespace GithookPreCommit
 {
     /// <summary>
-    /// $Id: 854a55b006fdb114319899853b5c688a13c9d69d byte2702 byte2702 2024-09-22T17:00:11.0000000+02:00 (prev commit) $
+    /// $Id: 80b72888775b0d37f6cfb061acd70a116e00e2b2 byte2702 byte2702 2024-09-22T17:02:49.0000000+02:00 (prev commit) $
     /// 
     /// Documentation: https://www.codeproject.com/Articles/1161290/Save-Yourself-Some-Troubles-with-TortoiseGit-Pre-c
     /// </summary>
@@ -203,7 +203,7 @@ namespace GithookPreCommit
                 string? commitAuthor = headCommit?.Author.Name;
                 string? commitCommitter = headCommit?.Committer.Name;
                 DateTimeOffset commitCommitterWhen = headCommit?.Committer.When ?? DateTimeOffset.Now;
-                string result = string.Format("{0}Id: {1} {2} {3} {4} (prev commit) {0}",
+                string result = string.Format("{0}Id: {1} {2} {3} {4} (previous commit) {0}",
                     "$", commitId, commitAuthor, commitCommitter, commitCommitterWhen.ToString("o"));
                 return result;
             }
